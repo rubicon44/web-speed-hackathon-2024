03aab6aa5f7716426b641b886b8f278cda47b57b
rubicon44 <rub.b.c44@gmail.com>
2024-03-23 17:15:51 +0900

    [Change] 必要なmaterial-uiのIconだけ取得する

--- workspaces/app/src/features/icons/components/SvgIcon.tsx
+++ workspaces/app/src/features/icons/components/SvgIcon.tsx
@@ -1,14 +1,41 @@
-import * as Icons from '@mui/icons-material';
+import { NavigateNext } from '@mui/icons-material';
+import { ArrowBack } from '@mui/icons-material';
+import { Close } from '@mui/icons-material';
+import { Favorite } from '@mui/icons-material';
+import { FavoriteBorder } from '@mui/icons-material';
+import { Search } from '@mui/icons-material';
 
 type Props = {
   color: string;
   height: number;
-  type: keyof typeof Icons;
   width: number;
+  type: 'NavigateNext' | 'ArrowBack' | 'Close' | 'Favorite' | 'FavoriteBorder' | 'Search'; // アイコンのタイプを追加
 };
 
-export const SvgIcon: React.FC<Props> = ({ color, height, type, width }) => {
-  // eslint-disable-next-line
-  const Icon = Icons[type];
+export const SvgIcon: React.FC<Props> = ({ color, height, width, type }) => {
+  let Icon;
+
+  // アイコンの種類に応じて、適切なアイコンを選択する
+  switch (type) {
+    case 'NavigateNext':
+      Icon = NavigateNext;
+      break;
+    case 'ArrowBack':
+      Icon = ArrowBack;
+      break;
+    case 'Close':
+      Icon = Close;
+      break;
+    case 'Favorite':
+      Icon = Favorite;
+      break;
+    case 'FavoriteBorder':
+      Icon = FavoriteBorder;
+      break;
+    case 'Search':
+      Icon = Search;
+      break;
+  }
+
   return <Icon style={{ color, height, width }} />;
 };
